4860d7d70474bbb938c1c1d5db9f675336de3455
Christopher Collins <chris@spring215.com>
2014-07-23 12:58:39 -0400

    Fixed forms on CONTACT PAGE. Still need to copy them to others

--- affil_contact_form.php
+++ affil_contact_form.php
@@ -6,13 +6,13 @@ $contact_website = $_POST['contact_website'];
 $contact_phone = $_POST['contact_phone'];
 $contact_email = $_POST['contact_email'];
 $contact_message = $_POST['contact_message'];
-$to = "chris@spring215.com";
-$subject = "New Affilate Contact";
+$to = 'chris@spring215.com';
+$subject = 'New Affilate Contact';
 
-$message = 'CONTACT NAME: ' . $contact_name . 'COMPANY: ' . $contact_company . 'WEBSITE: ' $contact_website . 'PHONE: ' $contact_phone . 'EMAIL: ' $contact_email . 'MESSAGE: ' $contact_message;
+$message = 'CONTACT NAME: ' . $contact_name . "\n" . 'COMPANY: ' . $contact_company . "\n" . 'WEBSITE: ' . $contact_website . "\n" . 'PHONE: ' . $contact_phone . "\n" . 'EMAIL: ' . $contact_email . "\n" . 'MESSAGE: ' . $contact_message;
 
-mail ($to, $subject, $message, "From: " . $contact_name);
-echo "Your message has been sent.";
+mail ($to, $subject, $message, 'From: ' . $contact_name);
+echo 'Your message has been sent.';
 
 ?>
 
